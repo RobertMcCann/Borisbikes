2a8b33333cb7e4073959a6d9b3f2118fa4671d82
Robert <mccannb401@gmail.com>
2021-03-09 14:06:32 +0000

    added Gemfile, Error.rb files

---
+++ Borisbikes/error.rb
@@ -0,0 +1,6 @@
+docking_station = DockingStation
+
+### Causes error.rb:1:in `<main>': uninitialized constant DockingStation (NameError)
+### Ruby cannot find this object in Main (Main is the universe that is built when you run IRB)
+### Line number 1 uninitialized constant to solve we need to initialise the error by defining
+### DockingStation as something.
